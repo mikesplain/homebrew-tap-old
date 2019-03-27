7739a64c50f1dbadd81d40f61e8e64015a628781
mikesplain <mike.splain@gmail.com>
2019-03-27 10:32:23 -0400

    Move to folder

---
+++ Formula/kubectl-ns.rb
@@ -0,0 +1,18 @@
+class KubectlNs < Formula
+  desc "Kubectl-ns makes it eash to manage namespaces via kubectl"
+  homepage "https://github.com/weibeld/kubectl-ns"
+  url "https://raw.githubusercontent.com/weibeld/kubectl-ns/969a7e1e5234ead63f98c3cc532f25a54cd8b6f0/kubectl-ns"
+  sha256 "055e52462bfc35ef072d4894348100de3f1464cbd64fcaf013537faaf8c59195"
+  depends_on "kubernetes-cli"
+  depends_on "fzf"
+  version "1.0.0"
+
+  def install
+    bin.install "kubectl-ns"
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl ns -h 2>&1")
+    assert_match "Interactively change the current namespace.", run_output
+  end
+end
